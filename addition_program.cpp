e73982b657705afc7bf84c27fc0f9648304109e0
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-02-04 11:36:14 +0530

    Addition Program in CPP

---
+++ addition_program.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1;
+	int num2;
+	cout << "Enter Your First Number : " ;
+	cin >> num1;
+	cout << "Enter Your Second Number : ";
+	cin >> num2;
+
+	// Final SOlution
+	
+	cout << "The Addition of "<<num1<< "and "<<num2<<" is : "<<num1+num2 ;
+
+	return 0;
+}
+
