3cb6afbb1004cd5ebce99fa2b9c1ddbaa856694d
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-02-04 12:06:28 +0530

    struct_with_forloop

--- struct.cpp
+++ struct.cpp
@@ -12,9 +12,16 @@ int main() {
 		};
 	
 	Student s[2];
-	cout << "Enter Details of 2 Students  : " ;
-	for(int i=0 ; i<2 ; i++)
-		cin >> s[i].name >> s[i].roll_no >> s[i].age ;
+	cout << "Enter Details of 2 Students  : "<<endl ;
+	for(int i=0 ; i<2 ; i++){
+		cout << "Enter Name of Student No." <<i+1<< ": "<<endl;
+		cin >> s[i].name;
+		cout << "Enter Roll Number of Student No."<<i+1<<" : "<<endl;
+		cin >> s[i].roll_no;
+		cout << "Enter Age of Student No."<<i+1<<":"<<endl;
+		cin >> s[i].age ;
+	}
+	
 	for(int i=0 ; i<2 ; i++)
 		cout << s[i].name << s[i].roll_no << s[i].age ;
 
