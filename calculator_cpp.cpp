ea6eb34b4ea4dcded7fbf2699718f981938c4402
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-02-08 10:01:35 +0530

    Calculator in CPP

---
+++ calculator_cpp.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+class calculator{
+        public:
+        int sum(int first , int second)
+        {
+            return(first+second);
+        }
+        int sub(int first , int second)
+        {
+            return(first+second);
+        }
+        int mul(int first , int second)
+        {
+            return(first*second);
+        }
+        int divi(int first, int second)
+        {
+            return(first/second);
+        }
+    };
+int main()
+{
+    calculator obj;
+    int first,second;
+    cout<<"================== Calculator ===================";
+    cout<<"Input First Number  : ";
+    cin>>first;
+    cout<<"Input Second Number : ";
+    cin>>second;
+    cout<<"What Operation you like to Perform";
+    cout<<"1. Add"<<endl<<"2.Subtract"<<endl<<"3.Multiplication"<<endl<<"4.Division"<<endl;
+    int opt;
+    cin>>opt;
+    if (opt==1){
+        cout<<"Sum of Two Numbers is : "<<obj.sum(first,second);
+    }
+    else if (opt==2){
+        cout<<"Sub of Two Numbers is : "<<obj.sub(first,second);
+    }
+    else if (opt==3){
+        cout<<"Multiplication of Two Numbers is : "<<obj.mul(first,second);
+    }
+    else if (opt==4){
+        cout<<"Division of Two Numbers is : "<<obj.divi(first,second);
+    }
+
+    return 0;
+}
+
+
