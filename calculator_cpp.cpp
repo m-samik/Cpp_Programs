af16072d3e7df8328f18ad6e3791e13dae51788a
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-02-08 10:24:02 +0530

    Calculator in CPP

--- calculator_cpp.cpp
+++ calculator_cpp.cpp
@@ -10,7 +10,7 @@ class calculator{
         }
         int sub(int first , int second)
         {
-            return(first+second);
+            return(first-second);
         }
         int mul(int first , int second)
         {
