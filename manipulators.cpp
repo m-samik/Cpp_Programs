46767f0802fd854964a83c1a2545d21f0a444628
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-02-08 11:00:46 +0530

    Manipulators in CPP

---
+++ manipulators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    cin>>ws>>s1>>ws>>s2;
+    cout<<setfill('*')<<setw(10)<<s1<<endl<<s2;
+
+
+    return 0;
+}
